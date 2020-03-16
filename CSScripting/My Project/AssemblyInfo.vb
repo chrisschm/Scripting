eb908544a28c47f22e7d6a5e8889eadc1c5c92e9
Christian Schmidt <47923480+chrisschm@users.noreply.github.com>
2020-03-16 03:33:39 +0100

    Add files via upload

---
+++ CSScripting/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("CSScripting")>
+<Assembly: AssemblyDescription("Bibliothek zur Nutzung in Scripts")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CSScripting")>
+<Assembly: AssemblyCopyright("Copyright © 2020 Christian Schmidt")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("d0435698-5a80-44ef-92d8-96135a563229")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' indem Sie "*" wie unten gezeigt eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("0.1.1.15")>
+<Assembly: AssemblyFileVersion("0.1.1.15")>
